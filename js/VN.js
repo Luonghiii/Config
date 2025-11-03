ee1a1b3cd662d670a4e6b0bc62d77c0694d6e22f
Luonghiii <luonghii123@gmail.com>
2025-11-03 15:38:01 +0700

    Add files via upload

--- js/VN.js
+++ js/VN.js
@@ -7,16 +7,17 @@ luong = {
       "status": 1,
       "is_free_trial": false,
       "active": true,
-      "product_identifier": "Product_Auto_Renew_Annual",        
+      "product_identifier": "Product_Auto_Renew_Annual_2025_01_11",        
       "enabled_auto_renew": true,                     
       "is_gift_subscription": false,
       "platform": "iOS",
-      "billing_date_ms": 1180396800000,
-      "original_purchase_date_ms": 1180396800000,
-      "start_date_ms": 1180396800000,
-      "expires_date_ms": 4092599349000,
+      "billing_date_ms": 1761955200000,
+      "original_purchase_date_ms": 1761955200000,
+      "start_date_ms": 1761955200000,
+      "expires_date_ms": 3389472000000,
       "group_identifier": "20936308",
-      "origin_transaction_id": "0000000000"     
+      "origin_transaction_id": "490000029052007
+"     
     }
   ],
   "code": 1
