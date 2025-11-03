78682c4ccad2421ba600c9b8c037a5fafff0ef13
Luonghiii <luonghii123@gmail.com>
2025-11-03 22:17:27 +0700

    Add files via upload

---
+++ js/Perfect365.js
@@ -0,0 +1,14 @@
+var obj = JSON.parse($response.body);
+
+if (obj && obj.data && obj.data.subscribing === false) {
+  obj.data = {
+    ...obj.data,
+    "yearlyConversion": true,
+    "eligible": true,
+    "productId": "subscription_yearly",
+    "expireTime": "3337645200000",
+    "subscribing": true
+  };
+}
+
+$done({ body: JSON.stringify(obj) });
