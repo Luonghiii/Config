f1e08b5e01167048c4a091d4b4994c270afa6b7f
Luonghiii <luonghii123@gmail.com>
2025-11-04 15:04:04 +0700

    Add files via upload

---
+++ js/Faceswapper.js
@@ -0,0 +1,16 @@
+var obj = JSON.parse($response.body);
+
+obj.data.list = [{
+  "startTime" : 1180396800000,
+  "orderId" : "340001399999999",
+  "isTrialPeriod" : true,
+  "endTime" : 3389472000000,
+  "productId" : "73_premium_normal_yearly",
+  "productType" : 3,
+  "orderStatus" : 1,
+  "autoRenewStatus" : true,
+  "originalOrderId" : "340001399999999",
+  "sign" : "c5e5450b552ac10149dcd7d4625b1ad2"
+}];
+
+$done({body : JSON.stringify(obj)});
