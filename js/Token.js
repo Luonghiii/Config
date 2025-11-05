4ba30c005a0eb5328aba7a8303005cc9b8ff18fb
Luonghiii <luonghii123@gmail.com>
2025-11-06 01:30:42 +0700

    Delete js/Token.js

--- js/Token.js
+++
@@ -1,3 +0,0 @@
-let headers = $request.headers
-headers["x-session-token"]= "F3S0w0bysBQFdbjtxpFurhCgz/gzHWsPz2TGi+hAkQmMU3hNDmGIhmnqLWHnbkKj1QmU7ygY0U52TvNsr9G6qhpJq8WZ7w3nkrOibXSo6y8e4I6gDP2BCRApfBM+h36LIGWfFnYfi+LNVTG4YAebCREwXox3RkLr1kIoW4vh2FHtn+q9adGPZUX5jxKTjpAHp/9ZVkkP23WVfg9c8hfkhQ==";
-$done({headers})
