c9a155afdcbc15624b32d383ffbbe73169c10c9c
Luonghiii <luonghii123@gmail.com>
2025-11-05 11:33:47 +0700

    Update Quizlet.js

--- js/Quizlet.js
+++ js/Quizlet.js
@@ -4,18 +4,15 @@ var obj=JSON.parse(body);
 if(obj.responses && obj.responses[0] && obj.responses[0].models && obj.responses[0].models.user){
     var user=obj.responses[0].models.user[0];
     
-    // Premium features
     user._isEligibleForFreeTrial=false;
-    user._featurePlanType="premium";
+    user._featurePlanType="Annual";
     user.type=1;
     
-    // Extra features
     user._isConfirmationRequired=false;
     user._canChangeUsername=true;
     user._isEligibleForStudentReferralsProgram=true;
     user._canSendDataFromGTMToAdNetworks=true;
-    
-    // Age restrictions
+
     user._isUnderAge=false;
     user._isUnderAgeForAds=false;
     user._needsChildDirectedTreatment=false;
