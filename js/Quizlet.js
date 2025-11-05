1092536e86de4039ecb0975854aa8be937150f48
Luonghiii <luonghii123@gmail.com>
2025-11-05 11:28:43 +0700

    Update Quizlet.js

--- js/Quizlet.js
+++ js/Quizlet.js
@@ -1,18 +1,24 @@
-re('"_isEligibleForFreeTrial":\\w+@"_featurePlanType":null@"type":0','"_isEligibleForFreeTrial":false@"_featurePlanType":"annual"@"type":1');
+var body=$response.body;
+var obj=JSON.parse(body);
 
-function re(){
-    var body=$response.body;
-    if(arguments[0].includes("@")){
-        var regs=arguments[0].split("@");
-        var strs=arguments[1].split("@");
-        for(i=0;i<regs.length;i++){
-            var reg=new RegExp(regs[i],"g");
-            body=body.replace(reg,strs[i]);
-        }
-    }
-    else{
-        var reg=new RegExp(arguments[0],"g");
-        body=body.replace(reg,arguments[1]);
-    }
-    $done({body});
+if(obj.responses && obj.responses[0] && obj.responses[0].models && obj.responses[0].models.user){
+    var user=obj.responses[0].models.user[0];
+    
+    // Premium features
+    user._isEligibleForFreeTrial=false;
+    user._featurePlanType="premium";
+    user.type=1;
+    
+    // Extra features
+    user._isConfirmationRequired=false;
+    user._canChangeUsername=true;
+    user._isEligibleForStudentReferralsProgram=true;
+    user._canSendDataFromGTMToAdNetworks=true;
+    
+    // Age restrictions
+    user._isUnderAge=false;
+    user._isUnderAgeForAds=false;
+    user._needsChildDirectedTreatment=false;
 }
+
+$done({body:JSON.stringify(obj)});
