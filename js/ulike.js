50c020615c8bacdf80061b7712410e1e7f420f08
Luonghiii <luonghii123@gmail.com>
2025-11-10 00:55:32 +0700

    Update ulike.js

--- js/ulike.js
+++ js/ulike.js
@@ -1,16 +1,29 @@
-var obj = JSON.parse($response.body);
+// ulike-purchase.js
+var obj = {};
+try {
+  obj = JSON.parse($response.body || '{}');
+} catch (e) {
+  obj = {};
+}
 
-// Set tất cả giá thành 0đ và bỏ trial
-obj.data.price_list.forEach(item => {
-  item.total_amount = 0;
-  item.price_tips = "0";
-  item.can_trial = true;
-  item.trial_tips = "Miễn phí vĩnh viễn";
-});
+// Ensure data structure
+obj.ret = "0";
+obj.errmsg = "success";
+obj.systime = obj.systime || Date.now().toString();
+obj.log_id = obj.log_id || "FAKE_" + Date.now();
 
-obj.data.unauto_price_list.forEach(item => {
-  item.total_amount = 0;
-  item.price_tips = "0";
-});
+obj.data = obj.data || {};
+obj.data.has_more = false;
+obj.data.next_cursor = 0;
+obj.data.authkey = obj.data.authkey || "7ada8f7f363f517bd9ebb8eef7d2486c";
+
+// Fake premium subscription
+obj.data.list = [{
+  "product_id": "beauty_subscription_year",
+  "expire_time": 9999999999999,
+  "is_valid": true,
+  "purchase_time": Date.now(),
+  "order_id": "FAKE_ORDER_" + Date.now()
+}];
 
 $done({body: JSON.stringify(obj)});
