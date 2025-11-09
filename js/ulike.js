767e0d2375446c9c486405ac1d1af855046b532e
Luonghiii <luonghii123@gmail.com>
2025-11-10 00:47:50 +0700

    Update ulike.js

--- js/ulike.js
+++ js/ulike.js
@@ -1,11 +1,16 @@
 var obj = JSON.parse($response.body);
 
-obj.data.list = [{
-  "product_id": "beauty_subscription_year",
-  "expire_time": 9999999999999,
-  "is_valid": true,
-  "premium_status": "ACTIVE",
-  "auto_renew": true
-}];
+// Set tất cả giá thành 0đ và bỏ trial
+obj.data.price_list.forEach(item => {
+  item.total_amount = 0;
+  item.price_tips = "0";
+  item.can_trial = true;
+  item.trial_tips = "Miễn phí vĩnh viễn";
+});
+
+obj.data.unauto_price_list.forEach(item => {
+  item.total_amount = 0;
+  item.price_tips = "0";
+});
 
 $done({body: JSON.stringify(obj)});
